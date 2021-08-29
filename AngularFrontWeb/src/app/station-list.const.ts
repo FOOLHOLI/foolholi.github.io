9882ae895718300354ec1cf3f752e2cb431d8ecf
johnliu <foolholi8211165@gmail.com>
2021-08-29 19:49:33 +0800

    Station

---
+++ AngularFrontWeb/src/app/station-list.const.ts
@@ -0,0 +1,127 @@
+export const stationList = [
+  {
+    "Station": "三重國小站",
+    "Destination": "南勢角站",
+    "UpdateTime": "2021-08-29T17:24:50.633"
+  },
+  {
+    "Station": "土城站",
+    "Destination": "頂埔站",
+    "UpdateTime": "2021-08-29T17:24:47.2"
+  },
+  {
+    "Station": "大坪林站",
+    "Destination": "新北產業園區站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "中山國中站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "內湖站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "內湖站",
+    "Destination": "動物園站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "古亭站",
+    "Destination": "松山站",
+    "UpdateTime": "2021-08-29T17:24:32.19"
+  },
+  {
+    "Station": "民權西路站",
+    "Destination": "南勢角站",
+    "UpdateTime": "2021-08-29T17:24:56.293"
+  },
+  {
+    "Station": "忠孝復興站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "忠孝復興站",
+    "Destination": "動物園站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "忠孝新生站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:56.25"
+  },
+  {
+    "Station": "忠義站",
+    "Destination": "象山站",
+    "UpdateTime": "2021-08-29T17:24:55.11"
+  },
+  {
+    "Station": "東門站",
+    "Destination": "蘆洲站",
+    "UpdateTime": "2021-08-29T17:24:50.633"
+  },
+  {
+    "Station": "松江南京站",
+    "Destination": "迴龍站",
+    "UpdateTime": "2021-08-29T17:24:56.293"
+  },
+  {
+    "Station": "南京復興站",
+    "Destination": "台電大樓站",
+    "UpdateTime": "2021-08-29T17:24:52.68"
+  },
+  {
+    "Station": "南港站",
+    "Destination": "亞東醫院站",
+    "UpdateTime": "2021-08-29T17:24:56.25"
+  },
+  {
+    "Station": "徐匯中學站",
+    "Destination": "蘆洲站",
+    "UpdateTime": "2021-08-29T17:24:50.633"
+  },
+  {
+    "Station": "景美站",
+    "Destination": "松山站",
+    "UpdateTime": "2021-08-29T17:24:32.19"
+  },
+  {
+    "Station": "港墘站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "新北產業園區站",
+    "Destination": "大坪林站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "新埔站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:47.2"
+  },
+  {
+    "Station": "萬芳醫院站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "萬芳醫院站",
+    "Destination": "動物園站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "葫洲站",
+    "Destination": "南港展覽館站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  },
+  {
+    "Station": "葫洲站",
+    "Destination": "動物園站",
+    "UpdateTime": "2021-08-29T17:24:48"
+  }
+];
